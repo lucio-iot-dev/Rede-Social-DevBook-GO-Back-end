6119ae7e0d89f762b1ff283a26309fba15371f02
lucio <you@example.com>
2022-03-22 10:33:56 -0300

    Metodos para validaçao de publicaçao

--- src/modelos/publicacao.go
+++ src/modelos/publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa uma publicação feita por um usuário
 type Publicacao struct {
@@ -11,4 +15,30 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadoEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+// Preparar vai chamar os métodos para validar e formatar o usuário recebido
+func (publicacao *Publicacao) Preparar() error {
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	publicacao.formatar()
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O titulo é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O conteúdo é obrigatório e não pode estar em branco")
+	}
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
