5bbea1ac9b6aa41d857b77f3d41f203ed21725e8
lucio <you@example.com>
2022-03-22 13:41:29 -0300

    Exclusao de publicaçao

--- src/controllers/publicacoes.go
+++ src/controllers/publicacoes.go
@@ -175,5 +175,45 @@ if publicacaoSalvaNoBanco.AutorID != usuarioID {
 
 // DeletarPublicacao exclui os dados de uma publicação
 func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+   	//PEGAR O USUARIOID QUE ESTA DENTRO DO TOKEN
+	usuarioID, erro := autenticacao.ExtrairusuarioID(r)
+	if erro != nil {
+	 respostas.Erro(w, http.StatusUnauthorized, erro)
+	 return 
+}
 
+// LER O ID QUE ESTA VINDO POR PARAMETRO
+parametros := mux.Vars(r)
+publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+if erro !=  nil {
+	respostas.Erro(w, http.StatusBadRequest, erro)
+	return
+}
+
+//ABRIR A CONEXÃO COM O BANCO DE DADOS
+db, erro := banco.Conectar()
+if erro != nil {
+	respostas.Erro(w, http.StatusInternalServerError, erro)
+	return
+}
+defer db.Close()
+
+repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+publicacaoSalvaNoBanco, erro := repositorio.BuscarPorID(publicacaoID)
+if erro != nil {
+	respostas.Erro(w, http.StatusInternalServerError, erro)
+	return
+}
+
+if publicacaoSalvaNoBanco.AutorID != usuarioID {
+	respostas.Erro(w, http.StatusForbidden, errors.New("Não é possível deletar uma publicação que não seja sua")) 
+     return
+	}
+
+	if erro = repositorio.Deletar(publicacaoID); erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+	return
+	}
+
+	respostas.JSON(w, http.StatusNoContent, nil)
 } 
--- src/repositorios/publicacoes.go
+++ src/repositorios/publicacoes.go
@@ -122,4 +122,20 @@ func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos
 	 return nil
 }
 
+func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare("delete from publicacoes where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+} 
+
+
+
 
